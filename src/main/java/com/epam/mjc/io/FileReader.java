04607e46ca9b75d11187727b9efe403d1ce4927d
boris <baizenbe@gmail.com>
2023-09-25 16:57:53 +0600

    read

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -8,6 +8,7 @@ public class FileReader {
 
     public Profile getDataFromFile(File file) {
         Profile person = new Profile();
+        StringBuilder bld = new StringBuilder();
         try {
             BufferedReader reader = new BufferedReader(new java.io.FileReader(file));
             String line;
@@ -21,15 +22,20 @@ public class FileReader {
                 String key = "";
                 String value = "";
                 while (text.charAt(i) != ':') {
-                    key += text.charAt(i);
+                    bld.append(text.charAt(i));
                     i++;
                 }
+                key = bld.toString();
+                bld.setLength(0);
                 i++;
                 i++;
                 while (text.charAt(i) != '\n') {
+                    bld.append(text.charAt(i));
                     value += text.charAt(i);
                     i++;
                 }
+                value = bld.toString();
+                bld.setLength(0);
                 i++;
                 switch (key) {
                     case "Name":
