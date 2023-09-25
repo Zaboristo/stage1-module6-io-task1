c423c6e56ab8d9127a7d489707ed8da474069985
boris <baizenbe@gmail.com>
2023-09-25 17:11:05 +0600

    read

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -58,7 +58,7 @@ public class FileReader {
             }
 
         } catch (IOException e) {
-            throw new RuntimeException(e);
+            e.printStackTrace();
         }
         return person;
     }
